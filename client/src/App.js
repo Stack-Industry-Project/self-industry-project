e676c20470ef6783ec96107356e33f611842c801
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-06-03 21:42:43 -0400

    add scripts and add component folder and empty function

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestFunction from './components/calculator'
+
+function App() {
+  return (
+    <div>
+      <TestFunction />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/calculator.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const TestFunction = () => {
+  return (
+    <div>
+      <h1>working</h1>
+    </div>
+  )
+}
+
+export default TestFunction
