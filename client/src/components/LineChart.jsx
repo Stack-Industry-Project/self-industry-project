958b3c58ac259f7bcf25615578b4de4cbe6cf5a5
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-07-03 14:22:18 -0400

    made some changes to the chart

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Line, Scatter } from 'react-chartjs-2';
+
 //import Calculator and Calculation
 
     //allows you to set a range between two numbers
     const Range = (start,end) => {
         return Array(end - start + 1).fill().map((_, idx) => start + idx)
     }
-
+    const image = new Image();
+    image.src = '/Users/chadwickmini/Desktop/Self-retire/self-industry-project/client/src/401kphoto.jpeg';
     
+
+const plugin = {
+  id: 'custom_canvas_background_image',
+  beforeDraw: (chart) => {
+    if (image.complete) {
+      const ctx = chart.ctx;
+      const {top, left, width, height} = chart.chartArea;
+      const x = left + width / 2 - image.width / 2;
+      const y = top + height / 2 - image.height / 2;
+      ctx.drawImage(image, x, y);
+    } else {
+      image.onload = () => chart.draw();
+    }
+  }
+};
     export default function LineChart(props) {
         const labels = props.data.map(L => L.currentAge)
-        const labels2 = props.data.map(L => L.currentAge)
         const totalSavings = props.data.map(S => S.currentRetirementSavings)
         const totalContributed = props.data.map(TC => TC.totalAmountContributed)
         console.log(totalSavings)
-    
         const data = {
             labels: labels,
             datasets: [
@@ -23,26 +38,55 @@ import { Line } from 'react-chartjs-2';
                 data: totalSavings,
                 backgroundColor: "#FF8C00",
                 borderColor: "#FF8C00",
-                fill: true
+                hoverBorderWidth: 35,
+                point: false,
+                pointStyle:'circle',
+                stepped: false,
+                plugins: [plugin],
+                fill: false,
+                borderDash: [2000,2200],
+                elements: {
+                  point: {
+                    radius: 0
+                  }
+                },
+                animations: {
+                  tension: {
+                    duration: 100,
+                    easing: 'easeInOutExpo',
+                    from: 1,
+                    to: 0,
+                    loop: true
+                  }
+                },
+
+                
+                
               },
               {
                 label: "Total Contribution",
+                type: 'line',
                 data: totalContributed,
+                showLine: false,
+                options: {
+                  indexAxis: 'y',
+                  scales: {
+                    x: {
+                      beginAtZero: false
+                    }
+                  }
+                },
                 fill: false,
                 borderColor: "#00ADEE",
                 backgroundColor: "#00ADEE",
-
-          
-                
-                fill: true
+                hoverBorderWidth: 35,
+              
               }
             ]
           };
-          
     return (
       <div className="graph">
         <Line data={data} />
-      
       </div>
     );
   }
