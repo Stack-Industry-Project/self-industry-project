e37f5c9e97564bedce369464bf092471040692b1
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-03 21:32:23 -0400

    add link highlight and fix link spacing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,21 +5,21 @@ const Navbar = () => {
     <div id="nav-container">
       <nav className="nav">
         <div className="nav-left">
-          <a href='https://www.self.inc/'>
+          <a className="nav-left-logo" href='https://www.self.inc/'>
             <img src='https://www.self.inc/static/Self_Logo_Header.svg' alt="" />
           </a>
           <div className="nav-left-links-container">
-            <button className="nav-link">LEARN</button>
-            <button className="nav-link">ABOUT</button>
-            <button className="nav-link">RESOURCES</button>
-            <button className="nav-link">GET $20</button>
-            <button className="nav-link">HELP</button>
+            <button className="nav-link link-highlight">Learn</button>
+            <button className="nav-link link-highlight">About</button>
+            <button className="nav-link link-highlight">Resources</button>
+            <button className="nav-link link-highlight">Get $20</button>
+            <button className="nav-link link-highlight">Help</button>
           </div>
         </div>
         <div className="nav-right">
-          <button className="nav-link">LOG IN</button>
+          <button className="nav-link link-highlight">Log in</button>
           <div className="sign-up-btn">
-            <a className="nav-link" href='https://www.self.inc/#quick-signup'>START BUILDING TODAY</a>
+            <a className="nav-link" href='https://www.self.inc/#quick-signup'>Start building today</a>
           </div>
         </div>
       </nav>
